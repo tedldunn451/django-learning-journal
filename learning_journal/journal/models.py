1691420c45312192dcbe574eb7a67a81f02a4f63
Ted Dunn <tldunn451@gmail.com>
2018-08-30 15:21:59 -0400

    Add Entry model to journal app and run migrations to create table

--- learning_journal/journal/models.py
+++ learning_journal/journal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=128)
+    created = models.DateField(auto_now_add=True)
+    time_spent = models.CharField(max_length=64)
+    learned = models.TextField()
+    resourses = models.TextField()
